4e46fad504e8f3e4fb9721b75e8c8fd78a22ec86
adamsquishy <46731834+adamsquishy@users.noreply.github.com>
2026-09-04 14:21:05 -0500

    Update queries.sql

--- CS1-ECommerce/queries.sql
+++ CS1-ECommerce/queries.sql
@@ -1,1 +1,12 @@
-
+select 
+    e.CUST_ID,
+    d.CUST_FIRST_NAME,
+    d.CUST_LAST_NAME,
+    e.Amount_sold,
+    p.Supplier_ID,
+    p.PROD_ID,
+    (c.unit_price - c.unit_cost) as Margins
+From SH.SALES e
+JOIN SH.Customers d on e.CUST_ID = d.CUST_ID
+JOIN SH.PRODUCTS p on e.PROD_ID = p.PROD_ID
+JOIN SH.COSTS c on e.PROD_ID = c.PROD_ID
